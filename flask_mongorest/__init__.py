2b7ffbf4cdd9d296339c007c5e018b185b63a43e
ttasterisco <ttasterisco@gmail.com>
2013-05-08 23:47:59 +0100

    fix url naming

--- flask_mongorest/__init__.py
+++ flask_mongorest/__init__.py
@@ -13,17 +13,15 @@ class MongoRest(object):
         def decorator(klass):
             document_name = klass.resource.document.__name__.lower()
             name = kwargs.pop('name', document_name)
-            url = kwargs.pop('url', '/%s/' % document_name)
+            url = kwargs.pop('url', '/%s/' % name)
             if self.url_prefix:
                 url = '%s%s' % (self.url_prefix, url)
             pk_type = kwargs.pop('pk_type', 'string')
             view_func = klass.as_view(name)
-            if List in klass.methods: 
+            if List in klass.methods:
                 self.app.add_url_rule(url, defaults={'pk': None}, view_func=view_func, methods=[List.method], **kwargs)
             if Create in klass.methods or BulkUpdate in klass.methods:
                 self.app.add_url_rule(url, view_func=view_func, methods=[x.method for x in klass.methods if x in (Create, BulkUpdate)], **kwargs)
             self.app.add_url_rule('%s<%s:%s>/' % (url, pk_type, 'pk'), view_func=view_func, methods=[x.method for x in klass.methods], **kwargs)
             return klass
         return decorator
-
-
